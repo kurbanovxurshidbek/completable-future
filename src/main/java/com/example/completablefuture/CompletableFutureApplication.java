93899cd41901894f1d478454ec0c91b730a83383
khurshid <shamsun.com@gmail.com>
2024-01-15 20:46:26 +0500

    initial commit

--- src/main/java/com/example/completablefuture/CompletableFutureApplication.java
+++ src/main/java/com/example/completablefuture/CompletableFutureApplication.java
@@ -3,6 +3,8 @@ package com.example.completablefuture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//https://www.callicoder.com/java-8-completablefuture-tutorial/
+
 @SpringBootApplication
 public class CompletableFutureApplication {
 
